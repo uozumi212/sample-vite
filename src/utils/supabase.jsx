ddd9f3950340297ae7e5f0a22830ab75a2efdad2
uozumi212 <kazu3278mana@gmail.com>
2024-09-22 20:59:55 +0900

    deploy時の修正

--- src/utils/supabase.jsx
+++ src/utils/supabase.jsx
@@ -1,11 +1,22 @@
 import { createClient } from '@supabase/supabase-js';
 
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  console.error('Supabase URL or Anon Key is missing');
+  throw new Error('Supabase configuration is incomplete');
+}
+
 // Create a single supabase client for interacting with your database
-export const supabase = createClient(
-  // process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  // process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-  // 'https://xyzcompany.supabase.co',
-  // 'public-anon-key'
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_ANON_KEY
-);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+// Create a single supabase client for interacting with your database
+// export const supabase = createClient(
+//   // process.env.NEXT_PUBLIC_SUPABASE_URL!,
+//   // process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+//   // 'https://xyzcompany.supabase.co',
+//   // 'public-anon-key'
+//   import.meta.env.VITE_SUPABASE_URL,
+//   import.meta.env.VITE_SUPABASE_ANON_KEY
+// );
