e15578a21d8093afeb90e929d76e9069ee80a69f
uozumi212 <kazu3278mana@gmail.com>
2024-09-22 15:42:45 +0900

    firebase 接続追加

--- src/utils/supabaseFunction.jsx
+++ src/utils/supabaseFunction.jsx
@@ -1,6 +1,19 @@
 import { supabase } from '../utils/supabase';
 
 export const getAllStudies = async () => {
-  const studies = await supabase.from('study-record').select('*');
-  return studies.data;
+  // const studies = await supabase.from('study-record').select('*');
+  const { data, error } = await supabase.from('study-record').select('*');
+  if (error) throw error;
+  return data;
+};
+
+export const addStudy = async (study) => {
+  const { data, error } = await supabase.from('study-record').insert(study).single();
+  if (error) throw error ;
+  return data;
+};
+
+export const deleteStudy = async (id) => {
+  const { error } = await supabase.from('study-record').delete().match({ id });
+  if (error) throw error ;
 };
